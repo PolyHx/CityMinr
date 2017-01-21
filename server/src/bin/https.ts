f4f005e944896d0564dc5af539bf2b05e13895f0
JulienDuf <dufresne_julien@hotmail.ca>
2017-01-21 11:29:08 -0500

    Add express project seed

---
+++ server/src/bin/https.ts
@@ -0,0 +1,70 @@
+
+import { Server } from "../app";
+import * as https from "https";
+import * as fs from "fs";
+var debug = require('debug');
+
+var app = new Server().app;
+
+var port = normalizePort(process.env.PORT || '8080');
+app.set('port', port);
+
+var option = {
+	ca: fs.readFileSync(''),
+	key: fs.readFileSync(''),
+	cert: fs.readFileSync('')
+};
+
+var server = https.createServer(option, app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
---
+++ server/src/route/info.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+
+module Route {
+
+    export class Info {
+
+        public router: express.Router;
+
+        constructor() {
+            this.router = express.Router();
+
+            this.router.get("/", this.info);
+        }
+
+        private info(req: express.Request, res: express.Response) {
+            
+            res.json({
+                version: process.env.VERSION,
+                run_config: process.env.NODE_ENV
+            });
+        }
+    }
+}
+
+export = Route;
