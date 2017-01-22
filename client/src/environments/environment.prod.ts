4ed6b993243ee1df578b37dbf19275ab5659cc39
SirKhepre <brandonroberge@hotmail.ca>
2017-01-22 10:35:52 -0500

    Environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: "http://cityminr.io:8080"
+  API_URL: "https://hackatown.io:8087"
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  API_URL: "http://localhost:8080"
+  API_URL: "http://localhost:8087"
 };
